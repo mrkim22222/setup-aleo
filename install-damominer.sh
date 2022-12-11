bdceb8d5fc1da92613c055b2bb616397fd5ccf12
mrkim22222 <119950747+mrkim22222@users.noreply.github.com>
2022-12-12 02:24:31 +0700

    Rename run.sh to install-damominer.sh

---
+++ install-damominer.sh
@@ -0,0 +1,1 @@
+wget -q -O setup-pool-damominer.sh https://raw.githubusercontent.com/mrkim22222/setup-aleo/main/setup-pool-damominer.sh && chmod +x setup-pool-damominer.sh && sudo /bin/bash setup-pool-damominer.sh
--- run.sh
+++
@@ -1,1 +0,0 @@
-wget -q -O setup-pool-damominer.sh https://raw.githubusercontent.com/mrkim22222/setup-aleo/main/setup-pool-damominer.sh && chmod +x setup-pool-damominer.sh && sudo /bin/bash setup-pool-damominer.sh
